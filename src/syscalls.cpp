95b54325133ccfdcdca699345760cfb36046812e
Alexander Entinger <cto@lxrobotics.com>
2022-06-24 11:21:02 +0200

    Fix: new is a reserved keyword for C++.

--- src/syscalls.cpp
+++ src/syscalls.cpp
@@ -160,10 +160,10 @@ int _stat(char *file, struct stat *st)
 	return 0;
 }
 
-int _link(char *old, char *new)
+int _link(char *old, char * /* new */)
 {
 	(void)old;
-	(void)new;
+	/* (void)new; */
 	errno = EMLINK;
 	return -1;
 }
